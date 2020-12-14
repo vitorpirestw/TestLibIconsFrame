a0cc35d36ad2562f05e6bae6a48e612ecc7c56a6
Vitor Pires <vitor.pires@thoughtworks.com>
2020-12-14 10:27:08 -0300

    Initial Commit

---
+++ TestLibIconsFrame/TestLibIconsFrame.h
@@ -0,0 +1,18 @@
+//
+//  TestLibIconsFrame.h
+//  TestLibIconsFrame
+//
+//  Created by LAVitor Pires on 14/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestLibIconsFrame.
+FOUNDATION_EXPORT double TestLibIconsFrameVersionNumber;
+
+//! Project version string for TestLibIconsFrame.
+FOUNDATION_EXPORT const unsigned char TestLibIconsFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLibIconsFrame/PublicHeader.h>
+
+
